8837670c5f1690d5bff9916eeba8fea9b2694def
adityak-bit <adityajsr1998@gmail.com>
2020-10-10 08:54:07 +0530

    [ADITYA] Added : displayBoard() to UC3

--- com/cg/examples/TicTacToeGame.java
+++ com/cg/examples/TicTacToeGame.java
@@ -1,20 +1,26 @@
 package com.cg.examples;
 
-import java.util.Scanner;
-
 public class TicTacToeGame {
 
 	private static char board[][] = new char[3][3];
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
-		Scanner userInput = new Scanner(System.in);
-		char userLetter = chooseUserLetter(userInput);
-		char computerLetter = (userLetter == 'X') ? 'O' : 'X';
+		int counter = 1;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = Character.forDigit(counter++, 11);
+			}
+		}
+		displayBoard(board);
 	}
 
-	private static char chooseUserLetter(Scanner userInput) {
-		System.out.println("Choose your letter X or O: ");
-		return userInput.next().toUpperCase().charAt(0);
+	private static void displayBoard(char board[][]) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(board[i][j] + "    ");
+			}
+			System.out.println();
+		}
 	}
 }
