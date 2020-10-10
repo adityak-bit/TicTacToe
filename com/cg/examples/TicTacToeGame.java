1614d8fc0345bf7ec997ad706eafd95ad953c638
adityak-bit <adityajsr1998@gmail.com>
2020-10-10 09:19:12 +0530

    [ADITYA] Added : replaceIndex() to UC4 to move to a desired location on board

--- com/cg/examples/TicTacToeGame.java
+++ com/cg/examples/TicTacToeGame.java
@@ -1,8 +1,11 @@
 package com.cg.examples;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	private static char board[][] = new char[3][3];
+	private static Scanner in;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
@@ -12,6 +15,12 @@ public class TicTacToeGame {
 				board[i][j] = Character.forDigit(counter++, 11);
 			}
 		}
+		in = new Scanner(System.in);
+		System.out.println("Enter your index choice from 1-9: ");
+		char indexChoice = in.next().charAt(0);
+		System.out.println("Enter userInput X or O: ");
+		char userInput = in.next().charAt(0);
+		replaceIndex(board, indexChoice, userInput);
 		displayBoard(board);
 	}
 
@@ -21,6 +30,17 @@ public class TicTacToeGame {
 				System.out.print(board[i][j] + "    ");
 			}
 			System.out.println();
+		}
+	}
+
+	private static void replaceIndex(char board[][], char find, char replace) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (board[i][j] == find) {
+					board[i][j] = replace;
+					return;
+				}
+			}
 		}
 	}
 }
